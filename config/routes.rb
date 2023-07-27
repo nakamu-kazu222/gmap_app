262ac007ace76770669fec88ffa7b681e4439a7a
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-07-27 17:10:40 +0900

    ルーティング設定を行った

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'maps/index'
+  root to: 'maps#index'
+  resources :maps, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
